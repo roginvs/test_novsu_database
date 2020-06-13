ed4fde7cd86e30e38bd94d11000e48843c69b3e4
Vasilii Rogin <vasilii@giosg.com>
2020-06-13 17:27:57 +0300

    Steps

---
+++ run.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+cat init.pgsql | docker exec -i psql psql -U postgres
+cat schema.pgsql data.pgsql | docker exec -i psql psql -U postgres rogin
+
+cat queries.pgsql | docker exec -i psql psql -U postgres rogin
