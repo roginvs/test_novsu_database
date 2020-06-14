7b8e3096d8102337c11f84c0b964078bfec1f91c
Vasilii Rogin <vasilii@giosg.com>
2020-06-14 16:36:33 +0300

    Fix run

--- run.sh
+++ run.sh
@@ -1,12 +1,26 @@
 
 #!/bin/bash
 
+echo "======== Initialization ======== "
 cat init.pgsql | docker exec -i psql psql -U postgres
-cat schema.pgsql data.pgsql | docker exec -i psql psql -U postgres rogin
 
+echo "======== Schema ======== "
+cat schema.pgsql | docker exec -i psql psql -U postgres rogin
+
+echo "======== Demo data ======== "
+cat data.pgsql | docker exec -i psql psql -U postgres rogin
+
+echo "======== Queries======== "
 cat queries.pgsql | docker exec -i psql psql -U postgres rogin
 
 
-cat trigger*.pgsql | docker exec -i psql psql -U postgres rogin
+for f in trigger*.pgsql; do
+  echo "======== Triggers $f ======== "
+  cat $f | docker exec -i psql psql -U postgres rogin
+done
 
-cat view*.pgsql | docker exec -i psql psql -U postgres rogin
+for f in view*.pgsql; do
+  echo "======== Views $f ======== "
+  cat $f | docker exec -i psql psql -U postgres rogin
+done
+
